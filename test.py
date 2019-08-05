c3c9772f8f856816138156db35cf5fe99dd6b273
Daniel Shapiro <dcshapiro@gmail.com>
2019-08-05 15:22:58 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 from lemay_ai_sidecar import lemay_ai_sidecar
 import os
-
 # DEFINE PRETRAINED MODEL
 fname_pretrained="crawl-300d-2M.vec"
 
 if not os.path.exists(fname_pretrained):
-    !wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
-    !unzip crawl-300d-2M.vec.zip
+    os.system("wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip")
+    os.system("unzip crawl-300d-2M.vec.zip")
 
 #THE FASTTEXT MODEL FROM THE PAPER WAS TRAINED USING VECSIZE 100 AND WINDOWSIZE 2
 model,fname_sidecar,fname_custom = lemay_ai_sidecar().extendEmbeddingModel(dataset_csv="dataset.csv",
