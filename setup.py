72dd628e3ebdde531a452f185ba970bfce17ecb7
Daniel Shapiro <dcshapiro@gmail.com>
2020-10-22 20:17:11 -0400

    Security update

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
 
     include_package_data=True,
 
-    install_requires=["pandas>=0.24.*","Keras>=2.2.4","scikit-learn>=0.21.3","scipy>=0.17.0","tensorflow>=1.15.2,<1.14.*","numpy>=1.16.*","gensim>=3.8.0","wget>=3.2"],
+    install_requires=["pandas>=0.24.*","Keras>=2.2.4","scikit-learn>=0.21.3","scipy>=0.17.0","tensorflow>=1.15.4","numpy>=1.16.*","gensim>=3.8.0","wget>=3.2"],
 
 )
